870f3e6fd490cc229db0852fa504a907f353fa1f
MAUNDU <78978450+maunduwebmaster@users.noreply.github.com>
2021-02-19 23:10:11 +0300

    Update index.blade.php

--- resources/views/spa/index.blade.php
+++ resources/views/spa/index.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>AvoRed E commerce</title>
+    <title>E-commerce</title>
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
